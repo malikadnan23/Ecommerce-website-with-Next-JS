455b2c3851d9702c50b7b561c63fdf1230525a2f
adnan saeed <121759955+malikadnan23@users.noreply.github.com>
2026-07-05 10:59:19 +0500

    some files are added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
-    Hello world
+    <h1 className="text-3xl font-bold text-accent">Hello World</h1>
     </>
   );
 }
---
+++ src/components/HeaderTop.tsx
@@ -0,0 +1,9 @@
+const HeaderTop = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HeaderTop
